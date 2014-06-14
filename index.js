0469ba3b69254d443a58e73a7f512fb53431aba7
thibauts <thibaut.seguy@gmail.com>
2014-06-14 17:18:58 +0200

    fixed indentation

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-module.exports.Controller 					= require('./lib/controllers/controller');
-module.exports.JsonController 				= require('./lib/controllers/json');
-module.exports.RequestResponseController 	= require('./lib/controllers/request-response');
-module.exports.ConnectionController 		= require('./lib/controllers/connection');
-module.exports.HeartbeatController 			= require('./lib/controllers/heartbeat');
-module.exports.ReceiverController 			= require('./lib/controllers/receiver');
-module.exports.MediaController 				= require('./lib/controllers/media');
+module.exports.Controller                 = require('./lib/controllers/controller');
+module.exports.JsonController             = require('./lib/controllers/json');
+module.exports.RequestResponseController  = require('./lib/controllers/request-response');
+module.exports.ConnectionController       = require('./lib/controllers/connection');
+module.exports.HeartbeatController        = require('./lib/controllers/heartbeat');
+module.exports.ReceiverController         = require('./lib/controllers/receiver');
+module.exports.MediaController            = require('./lib/controllers/media');
 
-module.exports.Client = module.exports.PlatformSender 	= require('./lib/senders/platform');
-module.exports.DefaultMediaReceiver 					= require('./lib/senders/default-media-receiver');
+module.exports.Client = module.exports.PlatformSender   = require('./lib/senders/platform');
+module.exports.DefaultMediaReceiver                     = require('./lib/senders/default-media-receiver');
