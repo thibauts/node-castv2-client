2ae99053b644ac4339f72b873de219255382594c
Amila Welihinda <amilajack@gmail.com>
2018-09-03 00:18:02 -0700

    Initial code upgrade

--- examples/queue.js
+++ examples/queue.js
@@ -1,15 +1,13 @@
-var PlatformSender = require('../index').PlatformSender;
-var DefaultMediaReceiver = require('../index').DefaultMediaReceiver;
-var scanner = require("chromecast-scanner");
-
-var util = require("util");
+import scanner from 'chromecast-scanner';
+import util from 'util';
+import { PlatformSender } from '../index';
+import { DefaultMediaReceiver } from '../index';
 
 function ondeviceup(host, callback) {
-
-  var client = new PlatformSender();
+  const client = new PlatformSender();
 
   client.connect(host).then(() => {
-    console.log('connected, launching app ...')
+    console.log('connected, launching app ...');
     const mediaList = [
       {
         autoplay: true,
@@ -18,8 +16,8 @@ function ondeviceup(host, callback) {
         activeTrackIds: [],
         playbackDuration: 2,
         media: {
-          contentId: "http://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/dash/BigBuckBunnyAudio.mp4",
-          contentType: "audio/mpeg",
+          contentId: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/dash/BigBuckBunnyAudio.mp4',
+          contentType: 'audio/mpeg',
           streamType: 'BUFFERED'
         }
       },
@@ -30,8 +28,8 @@ function ondeviceup(host, callback) {
         activeTrackIds: [],
         playbackDuration: 2,
         media: {
-          contentId: "http://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/dash/ElephantsDreamAudio.mp4",
-          contentType: "audio/mpeg",
+          contentId: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/dash/ElephantsDreamAudio.mp4',
+          contentType: 'audio/mpeg',
           streamType: 'BUFFERED'
         }
       },
@@ -42,28 +40,28 @@ function ondeviceup(host, callback) {
         activeTrackIds: [],
         playbackDuration: 2,
         media: {
-          contentId: "http://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/dash/ForBiggerBlazesAudio.mp4",
-          contentType: "audio/mpeg",
+          contentId: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/dash/ForBiggerBlazesAudio.mp4',
+          contentType: 'audio/mpeg',
           streamType: 'BUFFERED'
         }
       }
-    ]
+    ];
     client.launch(DefaultMediaReceiver).then((player) => {
-      console.log('app "%s" launched, loading medias...', player.session.displayName)
+      console.log('app "%s" launched, loading medias...', player.session.displayName);
       function gotStatus(status) {
-        console.log('status broadcast = %s', util.inspect(status), " ")
-        if (isDone, status.idleReason == "FINISHED" && status.loadingItemId === undefined) {
-          console.log("Done!")
-          callback(0)
+        console.log('status broadcast = %s', util.inspect(status), ' ');
+        if (isDone, status.idleReason == 'FINISHED' && status.loadingItemId === undefined) {
+          console.log('Done!');
+          callback(0);
         }
       }
-      player.on('status', gotStatus)
-      let isDone = false
+      player.on('status', gotStatus);
+      let isDone = false;
       player.queueLoad(mediaList, {
         startIndex: 1,
-        repeatMode: "REPEAT_OFF"
+        repeatMode: 'REPEAT_OFF'
       }).then((status) => {
-        console.log("Loaded QUEUE");
+        console.log('Loaded QUEUE');
         console.log(util.inspect(status));
         player.queueInsert([
           {
@@ -73,23 +71,23 @@ function ondeviceup(host, callback) {
             activeTrackIds: [],
             playbackDuration: 2,
             media: {
-              contentId: "http://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/dash/ForBiggerEscapesAudio.mp4",
-              contentType: "audio/mpeg",
+              contentId: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/dash/ForBiggerEscapesAudio.mp4',
+              contentType: 'audio/mpeg',
               streamType: 'BUFFERED',
               metadata: {
                 metadataType: 3,
-                title: "Original title"
+                title: 'Original title'
               }
             }
           }
         ]).then((status) => {
-          console.log('Inserted in queue')
-          console.log(util.inspect(status))
-          //Get status before we modify the title bellow
-          player.getStatus().then(gotStatus).catch((err) => console.error(err))
+          console.log('Inserted in queue');
+          console.log(util.inspect(status));
+          // Get status before we modify the title bellow
+          player.getStatus().then(gotStatus).catch(err => console.error(err));
           player.queueRemove([2], { currentItemId: 0 }).then((status) => {
-            console.log('Removed from queue')
-            console.log(util.inspect(status))
+            console.log('Removed from queue');
+            console.log(util.inspect(status));
             player.queueUpdate([{
               itemId: 4,
               autoplay: true,
@@ -98,82 +96,80 @@ function ondeviceup(host, callback) {
               activeTrackIds: [],
               playbackDuration: 2,
               media: {
-                contentId: "http://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/dash/ForBiggerEscapesAudio.mp4",
-                contentType: "audio/mpeg",
+                contentId: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/dash/ForBiggerEscapesAudio.mp4',
+                contentType: 'audio/mpeg',
                 streamType: 'BUFFERED',
                 metadata: {
                   metadataType: 3,
-                  title: "Modified title"
+                  title: 'Modified title'
                 }
               }
             }], { currentItemId: 4 }).then((status) => {
-              console.log('Updated item')
-              console.log(util.inspect(status))
+              console.log('Updated item');
+              console.log(util.inspect(status));
               player.queueReorder([4, 3, 1], { currentItemId: 4 }).then((status) => {
                 console.log('Reordered queue');
                 console.log(util.inspect(status));
-                //Get status - check that the title from itemId=4 has been modified
+                // Get status - check that the title from itemId=4 has been modified
                 player.getStatus().then((status) => {
-                  gotStatus(status)
+                  gotStatus(status);
                   isDone = true;
                 }).catch((err) => {
-                  console.error('Status get error!', err)
-                  callback(err)
-                })
+                  console.error('Status get error!', err);
+                  callback(err);
+                });
               }).catch((err) => {
-                console.error('Queue reorder error!', err)
-                callback(err)
-              })
+                console.error('Queue reorder error!', err);
+                callback(err);
+              });
             }).catch((err) => {
-              console.error('Queue update error!', err)
-              callback(err)
-            })
+              console.error('Queue update error!', err);
+              callback(err);
+            });
           }).catch((err) => {
-            console.error('Queue removal error!', err)
-            callback(err)
-          })
+            console.error('Queue removal error!', err);
+            callback(err);
+          });
         }).catch((err) => {
-          console.error('Error!', err)
-          callback(err)
-        })
+          console.error('Error!', err);
+          callback(err);
+        });
       }).catch((err) => {
-        console.log(util.inspect(err))
-        callback(err) //Error
-        return
-      })
+        console.log(util.inspect(err));
+        callback(err); // Error
+      });
     }).catch((err) => {
-      console.error('Launch error!', err)
-      callback(err)
-    })
+      console.error('Launch error!', err);
+      callback(err);
+    });
   }).catch((err) => {
-    console.error('Connect error!', err)
-    callback(err)
-  })
-  
-  client.on('error', function (err) {
+    console.error('Connect error!', err);
+    callback(err);
+  });
+
+  client.on('error', (err) => {
     console.log('Error: %s', err.message);
     client.close();
-    callback(err); //Error
-  })
-
+    callback(err); // Error
+  });
 }
 
 function findAndConnect(callback) {
-  scanner(function(err, service) {
-    console.log('chromecast %s running on: %s', service.name, service.data)
-    ondeviceup(service.data, callback)
-  })
-}
-
-//module for testcase
-module.exports = findAndConnect;
-
-//main
-var main = function () {
-  findAndConnect(function (rc) {
-    process.exit(rc);
+  scanner((err, service) => {
+    console.log('chromecast %s running on: %s', service.name, service.data);
+    ondeviceup(service.data, callback);
   });
 }
+
+// module for testcase
+export default findAndConnect;
+
+// main
+const main = () => {
+  findAndConnect((rc) => {
+    process.exit(rc);
+  });
+};
 if (require.main === module) {
   main();
 }
--- test/basicTest.js
+++ test/basicTest.js
@@ -1,8 +1,8 @@
-var basicExample = require("../examples/basic");
+import basicExample from '../examples/basic';
 
-describe('Basic', function() {
-  it('base functions', function(done) {
-      this.timeout(60000); //It usually takes 10 seconds
-      basicExample(done);
+describe('Basic', () => {
+  it('base functions', function (done) {
+    this.timeout(60000); // It usually takes 10 seconds
+    basicExample(done);
   });
 });
