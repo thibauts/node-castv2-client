f39ad41d89672b850e4d6ad971823183e6ad3e00
Amila Welihinda <amilajack@gmail.com>
2018-09-08 22:04:20 -0700

    chore: migrate examples to async/await

--- test/queue.spec.js
+++ test/queue.spec.js
@@ -3,7 +3,7 @@ import queueExample from '../examples/queue';
 jest.setTimeout(60000); // It usually takes 25 seconds
 
 describe('Queue', () => {
-  it('dynamic playlist changes', (done) => {
-    queueExample(done);
+  it('dynamic playlist changes', async () => {
+    await queueExample();
   });
 });
